ab536717b524dd2c2b1a4bf48e9b1315ccb20904
Keishon Ross <keishonross97@gmail.com>
2018-11-12 20:23:21 +0000

    added authorization header

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,20 @@
 const request = require('request');
+const secrets = require('./secrets.js');
 
 console.log("Welcome to the Github Avatar Downloader!");
 
 function getRepoContributors(repoOwner, repoName, cb){
+    let options = {
+        'url': "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+        headers: {
+            'Authorization': secrets.GITHUB_TOKEN,
+            'User-Agent': 'request'
+        }
+    };
 
+    request(options.url, (err, res, body) => {
+        cb(err, body);
+    });
 }
 
 getRepoContributors("jquery", "jquery", (err, result) => {
