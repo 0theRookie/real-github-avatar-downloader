abfa21e784539464825e358f41889aa15c20efae
Keishon Ross <keishonross97@gmail.com>
2018-11-12 19:59:08 +0000

    added empty function to get repo contributors, with hardcoded test call

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 const request = require('request');
 
 console.log("Welcome to the Github Avatar Downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb){
+
+}
+
+getRepoContributors("jquery", "jquery", (err, result) => {
+    console.log("Errors:", err);
+    console.log("Result:", result);
+})
